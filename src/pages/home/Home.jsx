b60b02cba0aec8d9c0be7ec2df10daa32030cd9a
Pushpesh Almiya <almiyapushpesh55@gmail.com>
2023-10-03 22:18:10 +0530

    Folder Structure setup

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./style.scss"
+import HeroBanner from "./heroBanner/HeroBanner"
+function Home() {
+  return (
+    <>
+      <HeroBanner/>
+    </>
+  )
+}
+
+export default Home
---
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./style.scss"
+function HeroBanner() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default HeroBanner
