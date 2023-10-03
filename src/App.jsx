6b778e1d02f1ba95635523fe8e6a0e3b4793fcad
Pushpesh Almiya <almiyapushpesh55@gmail.com>
2023-10-03 22:29:49 +0530

    React Router setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,15 @@ import { useEffect } from "react";
 import { fetchDataFromApi } from "./utils/api";
 import { useSelector, useDispatch } from "react-redux";
 import { getApiConfiguration, getGenres } from "./store/slices/homeSlice";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Home from "./pages/home/Home";
+import Details from "./pages/details/Details";
+import SearchResult from "./pages/searchResult/SearchResult";
+import Explore from "./pages/explore/Explore";
+import ErrorPage from "./pages/404/ErrorPage";
 
 function App() {
   const dispatch = useDispatch();
@@ -18,7 +27,21 @@ function App() {
   useEffect(() => {
     apiTesting();
   }, []);
-  return <>Welcome to Movix....... {url?.total_pages}</>;
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/:mediaType/:id" element={<Details />} />
+          <Route path="/search/:query" element={<SearchResult />} />
+          <Route path="/explore/:mediaType" element={<Explore />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
