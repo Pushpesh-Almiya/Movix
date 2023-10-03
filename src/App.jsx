0d361e0b1d52fd09481bae189f1ae3ae6ded4d9a
Pushpesh Almiya <almiyapushpesh55@gmail.com>
2023-10-03 21:59:16 +0530

    Redux toolkit setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
-import { useEffect } from "react"
-import { fetchDataFromApi } from "./utils/api"
+import { useEffect } from "react";
+import { fetchDataFromApi } from "./utils/api";
+import { useSelector, useDispatch } from "react-redux";
+import { getApiConfiguration, getGenres } from "./store/slices/homeSlice";
 function App() {
+  const dispatch = useDispatch();
+  const { url } = useSelector((state) => state.home);
+  // console.log(url);
+
   //API testing ....
-  const apiTesting =()=>{
-    fetchDataFromApi("/movie/popular").then((res)=>{
-      console.log(res)
-    })
-  }
-  useEffect(()=>{
+  const apiTesting = () => {
+    fetchDataFromApi("/movie/popular").then((res) => {
+      console.log(res);
+      dispatch(getApiConfiguration(res));
+    });
+  };
+  useEffect(() => {
     apiTesting();
-  },[])
-  return (
-    <>
-    Welcome to Movix.......
-    </>
-  )
+  }, []);
+  return <>Welcome to Movix....... {url?.total_pages}</>;
 }
 
-export default App
+export default App;
